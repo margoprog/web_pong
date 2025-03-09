429e1d10a266ada34946ca136f181e5d1c06e24e
Margaux Herault <gomar@KoobCam.local>
2025-03-09 18:18:25 +0100

    nouveau modèle méduse, ajout transparenc et particule sans déplacement

--- src/page/page3.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three';
-
-export function loadPage3(scene) {
-    const geometry = new THREE.ConeGeometry(1, 2, 32);
-    const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    const cone = new THREE.Mesh(geometry, material);
-    scene.add(cone);
-}
